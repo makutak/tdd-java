32b3419f1836fd8719f1a633e65eef2d93cbfd19
makutak <kounotakuma@realglobe.jp>
2017-11-05 18:31:01 +0900

    共通している箇所をMoneyクラスに移し、重複を消した。

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -1,16 +1,11 @@
 package money;
 
 class Dollar extends Money {
-    private int amount;
     Dollar(int amount) {
         this.amount = amount;
     }
 
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
-    }
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
     }
 }
