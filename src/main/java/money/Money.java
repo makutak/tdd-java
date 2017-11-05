33ffecd881772c3ebb070ac05d5fd6a7e33f8f66
makutak <kounotakuma@realglobe.jp>
2017-11-05 18:23:07 +0900

    Moneyクラスを作って継承させる方向で

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,16 +1,4 @@
 package money;
 
-class Dollar{
-    private int amount;
-    Dollar(int amount) {
-        this.amount = amount;
-    }
-
-    Dollar times(int multiplier) {
-        return new Dollar(amount * multiplier);
-    }
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
-    }
+class Money {
 }
